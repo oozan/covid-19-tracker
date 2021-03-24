ad69c497fdd4bbbb55716e2b248944a95a7a3137
oozaryanci <oozayranci@gmail.com>
2021-03-24 17:10:21 +0200

    Created Material-UI Selectdown menu

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 
+import { FormControl, MenuItem, Select} from '@material-ui/core';
 import './App.css';
 
 function App() {
   return (
     <div className="app">
       <h1>covid 19 tracker</h1>
-
+      <FormControl className="app__dropdown">
+        <Select variant="outlined" value="abc">
+          <MenuItem value="worldwide">Worldwide</MenuItem>
+          <MenuItem value="worldwide">Option two</MenuItem>
+          <MenuItem value="worldwide">Option three</MenuItem>
+          <MenuItem value="worldwide">Option four</MenuItem>
+        </Select>
+      </FormControl>
       {/*Header*/}
        {/* Title + Select input dropdown field */}
 
