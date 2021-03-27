b3c8f30c0035557c07010c33aeed8fa4482d18a5
oozaryanci <oozayranci@gmail.com>
2021-03-27 16:59:17 +0200

    Country select list is created by using external API

--- src/App.js
+++ src/App.js
@@ -1,18 +1,36 @@
 
+import React, { useState, useEffect } from 'react';
 import { FormControl, MenuItem, Select} from '@material-ui/core';
 import './App.css';
 
 function App() {
+  const [countries, setCountries] = useState([]);
+
+  useEffect(() => {
+    const getCountriesData = async () => {
+      await fetch('https://disease.sh/v3/covid-19/countries')
+      .then((response) => response.json())
+      .then((data) => {
+        const countries = data.map((country) => ({
+          name: country.country,
+          value: country.countryInfo.iso2,
+        }));
+        setCountries(countries);
+      })
+    }
+    getCountriesData();
+  }, []);
   return (
     <div className="app">
       <div className="app__header">
       <h1>covid 19 tracker</h1>
       <FormControl className="app__dropdown">
         <Select variant="outlined" value="abc">
-          <MenuItem value="worldwide">Worldwide</MenuItem>
-          <MenuItem value="worldwide">Option two</MenuItem>
-          <MenuItem value="worldwide">Option three</MenuItem>
-          <MenuItem value="worldwide">Option four</MenuItem>
+          {
+            countries.map((country) => (
+              <MenuItem value={country.value}>{country.name}</MenuItem>
+            ))
+          }
         </Select>
       </FormControl>
       </div>
