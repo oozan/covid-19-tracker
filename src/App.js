401fcffd0a7b8c595f1c18b505e9f1d50fcfc860
oozaryanci <oozayranci@gmail.com>
2021-03-28 18:50:03 +0300

    imported info-box compnent

--- src/App.js
+++ src/App.js
@@ -1,53 +1,51 @@
 /* eslint-disable no-unused-vars */
 
-import React, { useState, useEffect } from 'react';
-import { FormControl, MenuItem, Select} from '@material-ui/core';
+import React, {useState, useEffect} from 'react';
+import {FormControl, MenuItem, Select} from '@material-ui/core';
 import './App.css';
+import InfoBox from './components/InfoBox';
 
-function App() {
-  const [countries, setCountries] = useState([]);
-  const {country, setCountry} = useState("");
+function App () {
+  const [countries, setCountries] = useState ([]);
+  const {country, setCountry} = useState ('');
 
-  useEffect(() => {
+  useEffect (() => {
     const getCountriesData = async () => {
-      await fetch('https://disease.sh/v3/covid-19/countries')
-      .then((response) => response.json())
-      .then((data) => {
-        const countries = data.map((country) => ({
-          name: country.country,
-          value: country.countryInfo.iso2,
-        }));
-        setCountries(countries);
-      })
-    }
-    getCountriesData();
+      await fetch ('https://disease.sh/v3/covid-19/countries')
+        .then (response => response.json ())
+        .then (data => {
+          const countries = data.map (country => ({
+            name: country.country,
+            value: country.countryInfo.iso2,
+          }));
+          setCountries (countries);
+        });
+    };
+    getCountriesData ();
   }, []);
   return (
     <div className="app">
       <div className="app__header">
-      <h1>covid 19 tracker</h1>
-      <FormControl className="app__dropdown">
-        <Select variant="outlined" value={country}>
-        <MenuItem value="worldwide">Worldwide</MenuItem>
-          {
-            countries.map((country) => (
+        <h1>covid 19 tracker</h1>
+        <FormControl className="app__dropdown">
+          <Select variant="outlined" value={country}>
+            <MenuItem value="worldwide">Worldwide</MenuItem>
+            {countries.map (country => (
               <MenuItem value={country.value}>{country.name}</MenuItem>
-            ))
-          }
-        </Select>
-      </FormControl>
+            ))}
+          </Select>
+        </FormControl>
       </div>
-          <div className="app__stats">
+      <div className="app__stats">
+        <InfoBox title="Coronavirus Cases"/>
+        <InfoBox title="Recovered"/>
+        <InfoBox title="Deaths"/>
+      </div>
 
-          </div>
-       {/* InfoBoxs */}
-       {/* InfoBoxs */}
-       {/* InfoBoxs */}
+      {/* Table */}
+      {/* Graph */}
 
-       {/* Table */}
-       {/* Graph */}
-
-        {/* Map */}
+      {/* Map */}
     </div>
   );
 }
