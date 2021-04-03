93561670aa87f483517a321476ae519e437a1322
oozaryanci <oozayranci@gmail.com>
2021-04-03 22:46:34 +0300

    created buildChartData helper function to calculate daily new cases

--- src/components/LineGraph.js
+++ src/components/LineGraph.js
@@ -1,24 +1,42 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable react/jsx-no-comment-textnodes */
-import React, { useState, useEffect } from 'react'
+import React, {useState, useEffect} from 'react';
 //import { Line } from 'react-chartjs-2';
 
-function LineGraph() {
-    const [data, setData] = useState({});
-    // https://disease.sh/v3/covid-19/historical/all?lastdays=120
+function LineGraph () {
+  const [data, setData] = useState ({});
+  // https://disease.sh/v3/covid-19/historical/all?lastdays=120
 
-    useEffect(() => {
-        fetch('https://disease.sh/v3/covid-19/historical/all?lastdays=120')
-        .then(response => response.json())
-        .then(data => {
-            console.log("DATA IS HERE -->", data)
-        })
-    }, [])
-    return (
-        <div>
-            <h1>////GRAPH\\\\\</h1>
-        </div>
-    )
+  
+  const buildChartData = (data, casesType = 'cases' ) => {
+    const chartData = [];
+    let lastDataPoint;
+    data[casesType].forEach (date => {
+      if (lastDataPoint) {
+        const newDataPoint = {
+          x: date,
+          y: data[casesType][date] - lastDataPoint,
+        };
+        chartData.push(newDataPoint)
+      }
+      lastDataPoint = data[casesType][date];
+    });
+    return chartData;
+  };
+
+  useEffect (() => {
+    fetch ('https://disease.sh/v3/covid-19/historical/all?lastdays=120')
+      .then (response => response.json ())
+      .then (data => {
+        console.log ('DATA IS HERE -->', data);
+      });
+  }, []);
+
+  return (
+    <div>
+      <h1>////GRAPH\\\\\</h1>
+    </div>
+  );
 }
 
-export default LineGraph
+export default LineGraph;
