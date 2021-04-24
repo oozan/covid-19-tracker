acb046e93d87687bfcaa40658b9629a66608868b
oozaryanci <oozayranci@gmail.com>
2021-04-24 21:16:18 +0300

    numeral package is installed

--- src/components/LineGraph.js
+++ src/components/LineGraph.js
@@ -1,5 +1,6 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable react/jsx-no-comment-textnodes */
+import numeral from 'numeral';
 import React, {useState, useEffect} from 'react';
 import { Line } from 'react-chartjs-2';
 
@@ -7,11 +8,56 @@ function LineGraph () {
   const [data, setData] = useState ({});
   // https://disease.sh/v3/covid-19/historical/all?lastdays=120
 
+  const options = {
+    legend: {
+      display: false,
+    },
+    elements: {
+      point: {
+        radius: 0,
+      }
+    },
+    maintainAspectRatio: false,
+    tooltips: {
+      mode: "index",
+      intersect: false,
+      callbacks: {
+        label: function(tooltipItem, data) {
+          return numeral(tooltipItem.value).format("+0,0");
+        },
+      },
+    },
+    scales: {
+      xAxes: [
+        {
+          type: "time",
+          time: {
+            format: "MM/DD/YY",
+            tooltipFormat: "ll",
+          },
+        },
+      ],
+      yAxes: [
+        {
+          gridLines: {
+            display: false,
+          },
+          ticks: {
+            // Include a dollar sign in the ticks
+            callback: function (value, index, values) {
+              return numeral(value).format("0a");
+            },
+          },
+        },
+      ],
+    },
+  };
+
   
   const buildChartData = (data, casesType = 'cases' ) => {
     const chartData = [];
     let lastDataPoint;
-    data[casesType].forEach (date => {
+    for (let date in data.cases) {
       if (lastDataPoint) {
         const newDataPoint = {
           x: date,
@@ -20,7 +66,7 @@ function LineGraph () {
         chartData.push(newDataPoint)
       }
       lastDataPoint = data[casesType][date];
-    });
+    }
     return chartData;
   };
 
